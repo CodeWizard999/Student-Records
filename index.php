9db127af5c4e6a73f094384370166975f62b5f6e
CodeWizard999 <fizza.imran.work@gmail.com>
2023-04-24 11:18:17 -0400

    added info

--- index.php
+++ index.php
@@ -18,13 +18,12 @@
             <br>
             <br>
 
-            <h2>Welcome</h2>
-            <p>
-                This is a is a Student record application, Authorized users can add to the record and search 
-                the record. 
-            </p>
-            <h4 style="color:047baa;">Please log in if you are an existing user</h4> 
-            <h4 style="color:047baa;">Register if you are not a privious user</h4>
+            <h2>Welcome!</h2>
+         
+            <p>Our application provides a comprehensive and efficient way to manage student records. With our user-friendly interface, you can easily add, update, and view student information such as grades, attendance, and personal details. Say goodbye to manual record-keeping and hello to streamlined and organized student management with our Student Record Application</p>
+          
+            <h5>Please log in if you are an existing user</h5> 
+            <h5>Register if you are not a privious user</h5>
             
 
         </div>
